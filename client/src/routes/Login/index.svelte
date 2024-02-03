6d94a777c819ce922c4563a5f72d012400cd1303
Andy Tran <anhquoctran006@gmail.com>
2024-02-03 13:37:09 -0700

    Finished setting up svelte-routing
    - run npm i -D svelte-routing in the client folder to install the package.
    - run the app normally

---
+++ client/src/routes/Login/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let propName = 'defaultValue';
+</script>
+
+<main>
+    <h1>Hello, this is the {propName} page!</h1>
+</main>
+
+<style>
+
+</style>
+  
---
+++ client/src/routes/Profile/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let id;
+</script>
+
+<main>
+    <h1>Hello, this is the {id} profile page!</h1>
+</main>
+
+<style>
+
+</style>
