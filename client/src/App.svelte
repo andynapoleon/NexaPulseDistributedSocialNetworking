d9121075e6d63a95e8254a351e222cb084500ce3
bohan <byao2@ualberta.ca>
2024-02-08 01:56:24 -0700

    moved messages to notifications, changed UI

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -8,17 +8,22 @@
   import Login from "./routes/login/Index.svelte";
   import Profile from "./routes/profile/Index.svelte";
   import UserMessage from "./routes/messages/UserMessage.svelte";
+  import Notifications from "./routes/Notifications/notifications.svelte";
   import { mode, userId, token } from "./stores/stores.js";
 
   let login = "Login";
   $userId = "123";
+
+  function clicked(){
+    alert('12312')
+  }
 
   const menuItems = [
     { href: "/", label: "Home" },
     { href: `/messages/${$userId}`, label: "Messages" },
     { href: "/makepost", label: "Create a post" },
     { href: "/friends", label: "Friends" },
-    { href: "/notifications", label: "Notifications" },
+    { href: `/notifications/${$userId}`, label: "Notifications" },
     { href: "/settings", label: "Settings" },
   ];
 
@@ -27,19 +32,23 @@
 
 <!-- Main Layout -->
 <SideBar items={menuItems} let:item>
-  {#if item.label === "Home"}
-    <Icon.HomeSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {:else if item.label === "Messages"}
-    <Icon.MessageCaptionSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {:else if item.label === "Create a post"}
-    <Icon.PlusSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {:else if item.label === "Friends"}
-    <Icon.ProfileCardOutline class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {:else if item.label === "Notifications"}
-    <Icon.BellActiveSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {:else if item.label === "Settings"}
-    <Icon.UserSettingsSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-  {/if}
+  <div>
+    <button on:click={clicked}>
+      {#if item.label === "Home"}
+        <Icon.HomeSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+      {:else if item.label === "Messages"}
+        <Icon.MessageCaptionSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+      {:else if item.label === "Create a post"}
+        <Icon.PlusSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+      {:else if item.label === "Friends"}
+        <Icon.ProfileCardOutline class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+      {:else if item.label === "Notifications"}
+        <Icon.BellActiveSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+      {:else if item.label === "Settings"}
+        <Icon.UserSettingsSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
+        {/if}
+    </button>
+  </div>  
 </SideBar>
 <NavBar />
 
@@ -55,9 +64,14 @@
     </Route>
     <Route path="/messages/:id" let:params>
       <div class="content">
-        <UserMessage id={$userId} />
+        <UserMessage/>
       </div>
     </Route>
+    <Route path="/notifications/:id" let:params>
+      <div class="content">
+        <Notifications id={$userId} />
+      </div>
+    </Route> 
   </div>
 </Router>
 
---
+++ client/src/routes/Notifications/notifications.svelte
@@ -0,0 +1,77 @@
+<script>
+  export let id;
+
+  let messDict = {
+    Scammer: "wants to follow you",
+    teacher: "wants to follow you",
+  };
+
+  function responseButtonClicked(response) {
+    alert(response);
+  }
+</script>
+
+<main id={id}>
+  {#each Object.entries(messDict) as [name, type]}
+    <div class="container">
+      <div>
+        <p class="custom-text">{name} {type}</p>
+      </div>
+      <div>
+        <button
+          class="custom-button-Yes"
+          on:click={() => responseButtonClicked("Yes")}>
+            Yes
+        </button>
+        <button
+          class="custom-button-No"
+          on:click={() => responseButtonClicked("No")}>
+          No
+        </button>
+      </div>
+    </div>
+  {/each}
+</main>
+
+<style>
+  .container {
+    border: 2px solid teal;
+    border-radius: 4px; /* Rounded corners */
+    padding: 4%;
+    height: 50px;
+    width: 600px;
+    background-color: teal; /* Background color */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+    margin-top: 2%;
+  }
+
+  .custom-text {
+    flex: 5;
+    font-size: 24px;
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+  }
+
+  .custom-button-Yes {
+    background-color: rgb(52, 197, 255);
+    color: rgb(255, 255, 255);
+    padding: 4px 12px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+
+  .custom-button-No {
+    background-color: rgb(190, 0, 0);
+    color: rgb(255, 255, 255);
+    padding: 4px 12px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
+</style>
