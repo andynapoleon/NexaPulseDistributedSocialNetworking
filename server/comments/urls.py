74f0c7e56582cc8e2008dcbcadefee9a6dc2b89a
Sheikh Safwan Hossain <sakifhossain71@gmail.com>
2024-02-22 14:50:34 -0700

    Update urls.py

--- server/comments/urls.py
+++ server/comments/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views # . referes to the current module we are in
 
+# path('authors/<str:author_id>/posts/<str:post_id>/comments/', views.CommentList.as_view(), name='comment-list'),
+
 urlpatterns = [
-    path('authors/{AUTHOR_ID}/posts/{POST_ID}/comments', views.get_comment, name='get_comment'),
-    path('service/api/author', views.create_comment, name='create_comment'),
+    path('authors/<str:author_id>/posts/<str:post_id>/comments', views.get_comment, name='get_comment'),
+    #path('service/api/author', views.create_comment, name='create_comment'),
 ]
