2e19a1466d26499d7bc20e3b1a122c537155abfd
Peter Thai <pwthai@ualberta.ca>
2024-02-23 15:15:28 -0700

    Update Post model

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('service/api/posts', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
-    path('service/api/authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='get_post'),
-    path('service/api/authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='update_post'),
-    path('service/api/authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='get_recent_posts'),
-    path('service/api/authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='create_post'),  # URL for creating a post
+    path('GET/authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='get_post'),
+    path('PUT/authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='update_post'),
+    path('GET/authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='get_recent_posts'),
+    path('PUT/api/authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='create_post'),  # URL for creating a post
 ]
