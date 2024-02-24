73696fc7d4b161c9eca14dac043172ec618c4069
Peter Thai <pwthai@ualberta.ca>
2024-02-23 19:20:47 -0700

    Finished API calls in backend for posts

--- server/posts/urls.py
+++ server/posts/urls.py
@@ -14,18 +14,9 @@ urlpatterns = [
     # Endpoint for retrieving the list of posts
     path('posts/', views.PostList.as_view(), name='post_list'),
 
-    # Endpoint for retrieving, updating, or deleting a specific post
-    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
-
     # Endpoint for retrieving a specific post by its author and post ID
-    path('authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='get_post'),
-
-    # Endpoint for updating a specific post by its author and post ID
-    path('authors/<str:author_id>/posts/<str:post_id>/update/', views.PostDetail.as_view(), name='update_post'),
+    path('authors/<str:author_id>/posts/<str:post_id>/', views.PostDetail.as_view(), name='get_post/update_post'),
 
     # Endpoint for retrieving recent posts by a specific author
-    path('api/authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='get_author_posts'),
-
-    # Endpoint for creating a new post by a specific author
-    path('authors/<str:author_id>/posts/create/', views.AuthorPosts.as_view(), name='create_post'),
+    path('authors/<str:author_id>/posts/', views.AuthorPosts.as_view(), name='get_author_posts/create_post'),
 ]
