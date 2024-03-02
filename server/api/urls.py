6e07a87d526b69903d3d21f13c453af281d28ae1
xuanvietchu <vietxuanchu657@gmail.com>
2024-03-02 14:47:46 -0700

    Add profile test case

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from . import views
 from auth.views import LoginView, TokenRefreshAPIView
+from authors.views import Profile
 from follow.views import FollowView, FollowAllView, UserFollowingView, UserFollowedView, UserFriendsView
 
 urlpatterns = [
     path('api/get/', views.getData),
-    path('api/profile/<int:user_id>', views.Profile.as_view()),
+    path('api/profile/<int:user_id>', Profile.as_view(), name="Profile"),
     path('api/add/', views.addAuthor),
     path("api/token/", LoginView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshAPIView.as_view(), name="token_refresh"),
--- server/authors/tests.py
+++ server/authors/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from .models import Author
+from django.urls import reverse
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
 
-# Create your tests here.
+class ProfileAPITestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = Author.objects.create(
+            firstName="John", lastName="Doe", github="https://github.com/johndoe", email="johndoe@example.com"
+        )
+        self.valid_user_id = self.user.id
+        self.invalid_user_id = self.valid_user_id + 1  # Non-existent user ID
+        
+        # Create an instance of Author and authenticate the client
+        self.test_author = Author.objects.create(firstName='testuser', lastName='testuser', github='https://github.com/testuser', email='testuser@example.com')
+        self.token = AccessToken.for_user(self.test_author)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.token))
+
+    def test_get_profile(self):
+        url = reverse("Profile", args=[self.valid_user_id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "full_name": "John Doe",
+                "github": "https://github.com/johndoe",
+                "email": "johndoe@example.com",
+            },
+        )
+
+    # Other test cases go here
