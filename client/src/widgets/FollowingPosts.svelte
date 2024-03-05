bb0818bc1bd6e10e2262acb7772791ee0fd63901
Andy Tran <anhquoctran006@gmail.com>
2024-03-05 15:34:44 -0700

    Fixed the bug of "For You" page and finished implementing public posts

---
+++ client/src/widgets/FollowingPosts.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from "svelte";
+  import Post from "./Post.svelte";
+  import { server } from "../stores/stores.js";
+  import { posts } from "../stores/stores.js";
+  import { authToken } from "../stores/stores.js";
+
+  // Function to fetch posts from the backend
+  async function fetchPosts() {
+    try {
+      const response = await fetch(server + "/api/following-posts/", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${$authToken}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Fetched posts:", data); // Log the fetched data
+        $posts = data;
+      } else {
+        console.error("Failed to fetch posts:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching posts:", error.message);
+    }
+  }
+
+  // Fetch posts when the component is mounted
+  onMount(() => {
+    fetchPosts();
+  });
+</script>
+
+<div class="posts">
+  {#each $posts as post (post.id)}
+    <Post {post} />
+  {/each}
+</div>
+
+<style>
+  .posts {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
