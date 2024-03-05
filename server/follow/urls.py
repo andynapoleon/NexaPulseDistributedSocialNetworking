a4a83009a479dd7588817942d7d5429a41a86afd
Andy Tran <anhquoctran006@gmail.com>
2024-03-05 16:57:28 -0700

    Fixed urls bug for friends/follows

--- server/follow/urls.py
+++ server/follow/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from follow.views import FollowView, FollowAllView, UserFollowingView, UserFollowedView, UserFriendsView
+from follow.views import (
+    FollowView,
+    FollowAllView,
+    UserFollowingView,
+    UserFollowedView,
+    UserFriendsView,
+)
 
 urlpatterns = [
-    path("api/follow/<int:user_id>", FollowView.as_view()),
-    path("api/follow/all/<int:user_id>", FollowAllView.as_view()),
-    path("api/friends/following/<int:user_id>", UserFollowingView.as_view()),
-    path("api/friends/followed/<int:user_id>", UserFollowedView.as_view()),
-    path("api/friends/friends/<int:user_id>", UserFriendsView.as_view()),
-
+    path("follow/<int:user_id>", FollowView.as_view()),
+    path("follow/all/<int:user_id>", FollowAllView.as_view()),
+    path("friends/following/<int:user_id>", UserFollowingView.as_view()),
+    path("friends/followed/<int:user_id>", UserFollowedView.as_view()),
+    path("friends/friends/<int:user_id>", UserFriendsView.as_view()),
 ]
