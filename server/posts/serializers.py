c1371166a5cb449d60456c1a8b68cb84bd3f2dde
Andy Tran <anhquoctran006@gmail.com>
2024-03-07 18:18:12 -0700

    Finished API for Sharing

--- server/posts/serializers.py
+++ server/posts/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = "__all__"
 
     def create(self, validated_data):
         """
