5ac934711821dd1fc8ea982512c8eacac33d8b1f
Andy Tran <anhquoctran006@gmail.com>
2024-03-13 11:59:45 -0600

    Made some progresss for Node

--- server/node/urls.py
+++ server/node/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("test/", views.test, name="test"),
-    # Add other URL patterns for handling likes, comments, etc.
-]
+urlpatterns = []
