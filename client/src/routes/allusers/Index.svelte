7bd815ef4df3bed785c98ff4ef28e467f7d885df
Andy Tran <anhquoctran006@gmail.com>
2024-03-15 19:30:36 -0600

    pushed

--- client/src/routes/allusers/Index.svelte
+++ client/src/routes/allusers/Index.svelte
@@ -1,55 +1,53 @@
 <script>
-    import { onMount } from "svelte";
-    import { authToken, server  } from "../../stores/stores.js";
+  import { onMount } from "svelte";
+  import { authToken, server } from "../../stores/stores.js";
 
-    onMount(async () => {
-        // fetch 
-        const allUsers = await getAllUsers();
+  onMount(async () => {
+    // fetch
+    const allUsers = await getAllUsers();
+  });
+
+  // Get all users
+  async function getAllUsers() {
+    const res_nodes = await fetch(server + "/api/nodes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${$authToken}`,
+        host: server,
+      },
     });
+    // Array of host, password, username
+    const nodes = await res_nodes.json();
 
-    // Get all users
-    async function getAllUsers() {
+    // get local authors from the server
+    const res = await fetch(server + "/api/authors", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${$authToken}`,
+      },
+    });
+    const res_json = await res.json();
+    const allAuthors = [];
 
-        const res_nodes = await fetch(server +"/api/nodes", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${$authToken}`,
-            },
-        });
-        // Array of host, password, username
-        const nodes = await res_nodes.json();
-
-        // get local authors from the server
-        const res = await fetch(server +"/api/authors", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${$authToken}`,
-            },
-        });
-        const res_json = await res.json();
-        const allAuthors = [];
-
-        // TODO:loop through other nodes to get remote authors
-        for (let node of nodes.items) {
-            const authString = `${node.username}:${node.password}`;
-            const encodedAuthString = btoa(authString);
-            const res2 = await fetch(node.host +"/api/authors", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Basic ${encodedAuthString}`,
-                },
-            });
-            const remoteAuthors = await res2.json();
-            if (remoteAuthors.items)
-                allAuthors.push(...remoteAuthors.items);
-        }
-        res_json.items.push(...allAuthors);
-        console.log(res_json);
-
-        return res_json;
-        
+    // TODO:loop through other nodes to get remote authors
+    for (let node of nodes.items) {
+      const authString = `${node.username}:${node.password}`;
+      const encodedAuthString = btoa(authString);
+      const res2 = await fetch(node.host + "/api/authors", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${encodedAuthString}`,
+        },
+      });
+      const remoteAuthors = await res2.json();
+      if (remoteAuthors.items) allAuthors.push(...remoteAuthors.items);
     }
+    res_json.items.push(...allAuthors);
+    console.log(res_json);
+
+    return res_json;
+  }
 </script>
--- server/auth/BasicOrTokenAuthentication.py
+++ server/auth/BasicOrTokenAuthentication.py
@@ -9,27 +9,26 @@ import socket
 class BasicOrTokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
         # Check if basic authentication credentials are provided
-        auth = request.headers.get('Authorization')
-        client_ip = request.META.get('REMOTE_ADDR', '')
-        sender_host = None
-        try:
-            sender_host = socket.gethostbyaddr(client_ip)[0]
-        except socket.herror:
-            sender_host = 'Unknown.com'
-        # print(sender_host)
-        sender_host = "http://" + sender_host + "/"
-        # print(auth)
+        auth = request.headers.get("Authorization")
+        sender_host = request.headers.get("host")
         print(sender_host)
         if auth:
             print("auth provided")
             # Parse basic auth header
             try:
                 auth_type, auth_string = auth.split()
-                if auth_type.lower() == 'basic':
-                    username, password = b64decode(auth_string.encode()).decode().split(':')
+                if auth_type.lower() == "basic":
+                    username, password = (
+                        b64decode(auth_string.encode()).decode().split(":")
+                    )
                     # print("Basic auth found", sender_host, username, password)
                     # check if the user exists in the database
-                    node = Node.objects.get(host=sender_host, username=username, password=password, isActive=True)
+                    node = Node.objects.get(
+                        host=sender_host,
+                        username=username,
+                        password=password,
+                        isActive=True,
+                    )
                     # if authentication is successful, return success response
                     if node:
                         print("Basic auth successful")
@@ -37,19 +36,21 @@ class BasicOrTokenAuthentication(BaseAuthentication):
                     else:
                         # If the user does not exist, return failure response
                         print("Basic auth failed")
-                        return AuthenticationFailed('Unauthorized')
-                        
+                        return AuthenticationFailed("Unauthorized")
+
             except:
                 pass  # If parsing fails or credentials are incorrect, proceed to token authentication
-        
+
         # If basic authentication fails or is not provided, try token authentication
         token_auth = JWTAuthentication()
         print("Trying token auth")
         try:
             user, token = token_auth.authenticate(request)
-            
+
             if user is None:
-                raise AuthenticationFailed('Unauthorized')  # Return unauthorized response
+                raise AuthenticationFailed(
+                    "Unauthorized"
+                )  # Return unauthorized response
             return user, token
         except:
-            raise AuthenticationFailed('Unauthorized')
+            raise AuthenticationFailed("Unauthorized")
