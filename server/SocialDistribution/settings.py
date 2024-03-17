4ca45aa00fb83c0c0650933286626127c02cbd1a
Andy Tran <anhquoctran006@gmail.com>
2024-03-17 14:22:06 -0600

    Backslash done

--- server/SocialDistribution/settings.py
+++ server/SocialDistribution/settings.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SERVER = "http://127.0.0.1:8000"
+SERVER = "http://127.0.0.1:8000/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
