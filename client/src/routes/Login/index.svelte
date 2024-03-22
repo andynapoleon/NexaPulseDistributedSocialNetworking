db3eb99a1c76f67fc9303c92ef614f89cba8b7ce
Andy Tran <anhquoctran006@gmail.com>
2024-03-22 15:15:32 -0600

    2131

--- client/src/routes/Login/index.svelte
+++ client/src/routes/Login/index.svelte
@@ -29,7 +29,7 @@
   async function handleLogin() {
     // Handle Login
     const loginEndpoint = server + "/api/token/";
-    console.log(loginEndpoint); 
+    console.log(loginEndpoint);
     const credentials = { email, password };
     try {
       const response = await fetch(loginEndpoint, {
@@ -46,7 +46,7 @@
       const data = await response.json();
       authToken.update((value) => (value = data.access));
       refreshToken.update((value) => (value = data.refresh));
-      console.log("data:", data)
+      console.log("data:", data);
       if (!data.is_active) {
         throw new Error("User not activated"); // Handle non-2xx responses
       }
@@ -63,7 +63,7 @@
     } catch (error) {
       errorMessage = error.message;
     }
-    
+
     // Handle creating remote copies
     const res_nodes = await fetch(server + "/api/nodes", {
       method: "GET",
@@ -75,26 +75,29 @@
     const nodes = await res_nodes.json();
     for (let node of nodes.items) {
       let authorData = {
-          id: $currentUser.userId,
-          displayName: $currentUser.name,
-          email: $currentUser.email,
-          password: "i450959540943809",
-          github: $currentUser.github,
-          profileImage: $currentUser.profileImage,
-          host: server,
-          isForeign: true
-        }
-        console.log("AUTHOR DATA", authorData)
-        const sendAuthorResponse = await fetch(
-          node.host + `/api/authors/new/`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(authorData),
-          }
-        );
+        id: $currentUser.userId,
+        displayName: $currentUser.name,
+        email: $currentUser.email,
+        password: "i450959540943809",
+        github: $currentUser.github,
+        profileImage: $currentUser.profileImage,
+        host: server,
+        isForeign: true,
+      };
+      console.log("AUTHOR DATA", authorData);
+      const sendAuthorResponse = await fetch(node.host + `/authors/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const getResponse = await fetch(server + `/api/authors/new/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(authorData),
+      });
     }
   }
 
@@ -102,15 +105,15 @@
     // Handle sign-up logic here
     const signUpEndpoint = server + "/api/authors/new/";
     console.log(signUpEndpoint);
-    const credentials = { 
-      "id": null,
-      "host": null,
-      "isForeign": false,
-      "email":email,
-      "password": password,
-      "displayName": displayName,
-      "github": github,
-      "profileImage": profileImage,
+    const credentials = {
+      id: null,
+      host: null,
+      isForeign: false,
+      email: email,
+      password: password,
+      displayName: displayName,
+      github: github,
+      profileImage: profileImage,
     };
     console.log(credentials);
     try {
@@ -131,7 +134,6 @@
     } catch (error) {
       errorMessage = error.message;
     }
-
   }
 
   function toggleForm() {
@@ -172,17 +174,45 @@
   {:else}
     <!-- Sign-up form -->
     <form class="login-form" on:submit|preventDefault={handleSignUp}>
-      <input type="text" class="login-input" placeholder="Display Name" bind:value={displayName} required />
-      <input type="email" class="login-input" placeholder="Email" bind:value={email} required/>
-      <input type="password" class="login-input" placeholder="Password" bind:value={password} required/>
-      <input type="profileImage" class="login-input" placeholder="Profile Image Url" bind:value={profileImage}/>
-      <input type="url" class="login-input" placeholder="Github" bind:value={github} required/>
+      <input
+        type="text"
+        class="login-input"
+        placeholder="Display Name"
+        bind:value={displayName}
+        required
+      />
+      <input
+        type="email"
+        class="login-input"
+        placeholder="Email"
+        bind:value={email}
+        required
+      />
+      <input
+        type="password"
+        class="login-input"
+        placeholder="Password"
+        bind:value={password}
+        required
+      />
+      <input
+        type="profileImage"
+        class="login-input"
+        placeholder="Profile Image Url"
+        bind:value={profileImage}
+      />
+      <input
+        type="url"
+        class="login-input"
+        placeholder="Github"
+        bind:value={github}
+        required
+      />
       <button type="submit" class="login-button">Sign Up</button>
       <div class="h-4"></div>
       <button class="login-button" on:click={toggleForm}>Back</button>
     </form>
   {/if}
-  
 </div>
 
 <style>
