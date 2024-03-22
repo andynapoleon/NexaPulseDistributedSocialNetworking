6c8aa5d1f11f685b4ba998254b934255444622ad
xuanvietchu <vietxuanchu657@gmail.com>
2024-03-21 19:38:29 -0600

    refactor remote post to inbox

--- server/SocialDistribution/settings.py
+++ server/SocialDistribution/settings.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SERVER = "https://nexapulse4-79e5b0bb2c15.herokuapp.com/"
+SERVER = "http://127.0.0.1:8000/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
