7874dc88c56eb9b633189d1fcd568e85c93ddb0e
xuanvietchu <vietxuanchu657@gmail.com>
2024-03-23 16:57:28 -0600

    viet deploy

--- server/SocialDistribution/settings.py
+++ server/SocialDistribution/settings.py
@@ -25,7 +25,7 @@ CORS_ALLOW_HEADERS = default_headers + (
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SERVER = "http://127.0.0.1:8000/"
+SERVER = "https://nexapulse-25959148b934.herokuapp.com/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
