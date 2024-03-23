d6012f427e085042544f3603e9b25d120abc66a0
Andy Tran <anhquoctran006@gmail.com>
2024-03-23 14:49:32 -0600

    changed

--- server/follow/views.py
+++ server/follow/views.py
@@ -63,22 +63,22 @@ class FollowView(APIView):
                 actor = AuthorSerializer(actor)
                 object = Author.objects.get(id=request.data["userId2"])
                 object = AuthorSerializer(object)
-                # data_to_send = {
-                #     "type": "Follow",
-                #     "summary": str(actor.data["displayName"])
-                #     + " wants to follow "
-                #     + str(object.data["displayName"]),
-                #     "actor": actor.data,
-                #     "object": object.data,
-                # }
                 data_to_send = {
                     "type": "Follow",
                     "summary": str(actor.data["displayName"])
                     + " wants to follow "
                     + str(object.data["displayName"]),
-                    "actor": "fdasfdas",
-                    "object": "fadsfdsf",
+                    "actor": actor.data,
+                    "object": object.data,
                 }
+                # data_to_send = {
+                #     "type": "Follow",
+                #     "summary": str(actor.data["displayName"])
+                #     + " wants to follow "
+                #     + str(object.data["displayName"]),
+                #     "actor": "fdasfdas",
+                #     "object": "fadsfdsf",
+                # }
                 print("DATA TO SEND", data_to_send)
                 requestObj = Follows.objects.filter(
                     follower_id=userId1, followed_id=userId2
