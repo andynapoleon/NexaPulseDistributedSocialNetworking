f1076fac2f7b45146400ee75a34fae89550700aa
Andy Tran <anhquoctran006@gmail.com>
2024-03-22 19:52:02 -0600

    completely revamped follow API for remote connection

--- server/authors/serializers.py
+++ server/authors/serializers.py
@@ -5,19 +5,16 @@ from .models import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['type', 'id', 'host', 'displayName', 'url', 'github', 'profileImage']
+        fields = ["type", "id", "host", "displayName", "url", "github", "profileImage"]
 
     def to_representation(self, instance):
-        '''Represents the id field as an url'''
+        """Represents the id field as an url"""
         data = super().to_representation(instance)
         print(data)
         context = self.context
-        base_url = context.get('base_url')
+        base_url = context.get("base_url")
         if base_url is not None:
             author_id = instance.id
-            data['id'] = f"{base_url}authors/{author_id}"
-            data['url'] = f"{base_url}authors/{author_id}"
-            data['host'] = f"{base_url}"
+            data["id"] = f"{base_url}authors/{author_id}"
+            data["url"] = f"{base_url}authors/{author_id}"
         return data
-
-   
