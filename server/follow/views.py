f6169817aa537c6bbee8f6fee659d23939203625
Andy Tran <anhquoctran006@gmail.com>
2024-03-22 20:08:35 -0600

    completely revamped follow API for remote connection

--- server/follow/views.py
+++ server/follow/views.py
@@ -127,7 +127,7 @@ class FollowView(APIView):
         if sender_host != receiver_host:
             userId2 = request.data.get("userId2")
             print("HOST", receiver_host)
-            print("ALL", Node.objects.all().first()
+            print("ALL", Node.objects.all().first())
             queryset = Node.objects.all().filter(
                 username="remote", password="123456", host=receiver_host
             )
