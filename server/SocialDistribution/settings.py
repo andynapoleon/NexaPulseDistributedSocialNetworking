6f6bdb026360d94b693f927b69399e987c43c320
Andy Tran <anhquoctran006@gmail.com>
2024-03-24 23:47:38 -0600

    Deploy

--- server/SocialDistribution/settings.py
+++ server/SocialDistribution/settings.py
@@ -19,13 +19,11 @@ from corsheaders.defaults import default_headers
 from datetime import timedelta
 
 
-CORS_ALLOW_HEADERS = default_headers + (
-    'Access-Control-Allow-Origin',
-)
+CORS_ALLOW_HEADERS = default_headers + ("Access-Control-Allow-Origin",)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SERVER = "https://nexapulse-25959148b934.herokuapp.com/"
+SERVER = "https://nexapulse1-7fbca99d2d7b.herokuapp.com/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
