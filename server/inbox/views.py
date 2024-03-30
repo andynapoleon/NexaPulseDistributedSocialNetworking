5f450dd2cfe310855fd1c7edbf01a84162ceb6d6
xuanvietchu <vietxuanchu657@gmail.com>
2024-03-30 16:07:59 -0600

    sharing to remote server

--- server/inbox/views.py
+++ server/inbox/views.py
@@ -142,7 +142,6 @@ class InboxView(APIView):
                     # fetch remote authors/${authorId}/posts/${postId}/image/",
                     if request_data["contentType"] == "application/base64" or "social-dist" in sender_host:
                         url_image = f"{sender_host}authors/{post_author_id}/posts/{post_id}/image"
-                        request_data["contentType"] = "text/plain"
                     else:
                         url_image = f"{sender_host}api/authors/{author_id}/posts/{post_id}/image/"
                     print("URL IMAGE", url_image)
@@ -217,7 +216,6 @@ class InboxView(APIView):
                         url_image = (
                             f"{sender_host}authors/{post_author_id}/posts/{id}/image"
                         )
-                        request_data["contentType"] = "text/plain"
                     else:
                         url_image = (
                             f"{sender_host}api/authors/{author_id}/posts/{id}/image/"
