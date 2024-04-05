7115544e43059dfb662e0021afa5c9243caa2aef
bohan <byao2@ualberta.ca>
2024-04-04 19:38:18 -0600

    Fixed dateformat for SharedPost

--- client/src/components/SideBar.svelte
+++ client/src/components/SideBar.svelte
@@ -3,14 +3,37 @@
   import { onMount, onDestroy } from 'svelte';
   import * as Icon from "flowbite-svelte-icons";
   import { writable } from 'svelte/store'; // Import writable store
-  import { navigate } from "svelte-routing"; // Assuming you're using svelte-routing for navigation
+  import { link, navigate } from "svelte-routing"; // Assuming you're using svelte-routing for navigation
   import { authToken, isLoginPage, currentUser } from "../stores/stores.js"; // Import currentUser from stores
+
+  let currentSelection;
 
   // Logout function
   function handleLogout() {
     $authToken = "";
     $isLoginPage = true;
     navigate("/");
+  }
+
+  function onClickHandler(link) {
+    navigate(link);
+    currentSelection = link;
+    // /home
+    // /foryou
+    // /friends/...
+    // /all_users
+    // /notifications/...
+  }
+
+  function returnCSSElementClass(current) {
+    console.log("currentSelection", currentSelection)
+    console.log("currentLabel", current)
+    if (current != currentSelection){
+      return "menu-item"
+    } else {
+      console.log('selcted')
+      return "selected-menu-item"
+    }
   }
 
   let sidebarClass = ""; // Initialize the sidebar class variable
@@ -31,14 +54,15 @@
     // Initial call
     updateSidebarClass()
     window.addEventListener('resize', updateSidebarClass);
+    window.addEventListener('click', returnCSSElementClass);
   });
 
   onDestroy(() => {
     window.removeEventListener('resize', updateSidebarClass);
   });
 
-  console.log('Window Width:', window.innerWidth);
-  console.log('Window Height:', window.innerHeight);
+  // console.log('Window Width:', window.innerWidth);
+  // console.log('Window Height:', window.innerHeight);
 
 </script>
 
@@ -48,49 +72,42 @@
       {#each items as item}
         <div class="container">
           {#if item.label === "Home"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
+            <button class={returnCSSElementClass("/home")} on:click={() => onClickHandler(`${item.href}`)}>
               <Icon.HomeSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
               {/if}
             </button>
           {:else if item.label === "For You"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
+            <button class={returnCSSElementClass("/foryou")} on:click={() => onClickHandler(`${item.href}`)}>
               <Icon.PhoneSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
               {/if}
             </button>
           {:else if item.label === "Friends"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
+            <button class={returnCSSElementClass("/friends")} on:click={() => onClickHandler(`${item.href}`)}>
               <Icon.ProfileCardOutline class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
               {/if}
             </button>
           {:else if item.label === "Everyone"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
+            <button class={returnCSSElementClass("/all_users")} on:click={() => onClickHandler(`${item.href}`)}>
               <Icon.UsersSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
               {/if}
             </button>
           {:else if item.label === "Requests"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
+            <button class={returnCSSElementClass("/notifications")} on:click={() => onClickHandler(`${item.href}`)}>
               <Icon.BellActiveSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
               {/if}
             </button>
-          {:else if item.label === "Settings"}
-            <button class="menu-item" on:click={() => navigate(`${item.href}`)}>
-              <Icon.UserSettingsSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
-              {#if sidebarClass === "bigSidebar"}
-                <span>{item.label}</span>
-              {/if}
-            </button>
           {:else if item.label === "Log Out"}
-            <button class="menu-item" on:click={handleLogout}>
+            <button class={returnCSSElementClass("/logout")} on:click={handleLogout}>
               <Icon.LockOpenSolid class="pt-3 w-[2em] h-[2em] text-[#C2C2C2]" />
               {#if sidebarClass === "bigSidebar"}
                 <span>{item.label}</span>
